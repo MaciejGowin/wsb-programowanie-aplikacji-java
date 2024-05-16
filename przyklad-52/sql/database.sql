656d4bc5d65de5b7f7437152dc6bd383f54afb6f
Maciej Gowin <maciej.abacus.gowin@gmail.com>
2024-05-16 23:51:38 +0200

    Update

--- przyklad-52/sql/database.sql
+++ przyklad-52/sql/database.sql
@@ -1,2 +1,2 @@
-CREATE DATABASE IF NOT EXISTS booking_system DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+CREATE DATABASE IF NOT EXISTS booking_system DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE booking_system;
--- przyklad-sql/booking_system.sql
+++ przyklad-sql/booking_system.sql
@@ -1,5 +1,5 @@
 DROP DATABASE IF EXISTS `booking_system`;
-CREATE DATABASE `booking_system` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+CREATE DATABASE `booking_system` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE booking_system;
 
 CREATE TABLE customers
